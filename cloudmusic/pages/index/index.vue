6e1890c803c84709b99910e5d0db4dffdeeb11e3
gachiBella <beiqu11dong111@gmail.com>
2023-02-08 16:20:49 +0800

    font

---
+++ cloudmusic/pages/index/index.vue
@@ -0,0 +1,60 @@
+<template>
+	<view class="content">
+		myapp
+		<span class="iconfont icon-fenxiang"></span> 
+		<svg class="icon" aria-hidden="true">
+		  <use xlink:href="#icon-aixin"></use>
+		</svg>
+	</view>
+</template>
+
+<script>
+	import '@/static/iconfont/iconfont.js'
+	export default {
+		data() {
+			return {
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.icon {
+	  width: 1em;
+	  height: 1em;
+	  vertical-align: -0.15em;
+	  fill: currentColor;
+	  overflow: hidden;
+	}
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
